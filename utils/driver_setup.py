a63764c3af93814bd6f177a5bac50c7aa2e3e643
ronik <ronikrawat@email>
2025-12-15 17:49:33 +0530

    second

--- utils/driver_setup.py
+++ utils/driver_setup.py
@@ -1,13 +1,19 @@
 from selenium import webdriver
+from selenium.webdriver.chrome.options import Options
+from selenium.webdriver.chrome.service import Service
+from webdriver_manager.chrome import ChromeDriverManager
 
 
-def get_driver(browser="chrome"):
-    """Initialize webdriver"""
-    if browser.lower() == "chrome":
-        driver = webdriver.Chrome()
-    elif browser.lower() == "firefox":
-        driver = webdriver.Firefox()
-    else:
-        driver = webdriver.Edge()
-    driver.maximize_window()
+def get_driver():
+    chrome_options = Options()
+    chrome_options.add_argument("--headless=new")
+    chrome_options.add_argument("--no-sandbox")
+    chrome_options.add_argument("--disable-dev-shm-usage")
+    chrome_options.add_argument("--disable-gpu")
+    chrome_options.add_argument("--window-size=1920,1080")
+
+    service = Service(ChromeDriverManager().install())
+    driver = webdriver.Chrome(service=service, options=chrome_options)
+    driver.implicitly_wait(10)
     return driver
+
